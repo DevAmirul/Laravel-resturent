b950b0edbe2791a7cd606fcc645c80a805fba113
Amirul islam <dev.amirul@gmail.com>
2023-03-04 22:51:11 +0600

    example

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,24 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get( '/', function () {
+    return view( 'welcome' );
+} );
+
+Route::get( 'hi', function () {
+
+    $collection = collect( [
+        'name' => 'Sally',
+        'school' => 'Arkansas',
+        'age' => 28,
+    ] );
+
+
+    $a = $collection->all() ;
+    return gettype($a);
+
+// ['name' => 'SALLY', 'school' => 'ARKANSAS', 'age' => '28'];
+
+} );
